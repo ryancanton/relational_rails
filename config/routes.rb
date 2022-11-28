2e5f946484a68d8142b865519733b5420694c1fb
Ryan Canton <ryancanton999@gmail.com>
2022-11-28 15:51:36 -0700

    Feat: Add views for /authors and /books

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
-  get '/authors' to: 'authors#index'
-  get '/books' to: 'books#index'
+  get '/authors', to: 'authors#index'
+  get '/books', to: 'books#index'
 end
