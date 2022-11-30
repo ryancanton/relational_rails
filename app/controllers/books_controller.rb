a8ccd374eb38ec552b37cb1901b153a319c44648
Ryan Canton <ryancanton999@gmail.com>
2022-11-30 15:44:33 -0700

    Test/Feat: Add test and functionality for user story 4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,5 +20,6 @@ class BooksController < ApplicationController
     end
 
     def show
+      @book = Book.find(params[:id])
     end
   end
---
+++ spec/features/books/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'The Book Show Page' do
+    it 'When I visit /books/:id I see the book with that id including the books attributes' do
+        author_1 = Author.create!(name: "Stephen King", rating: 7, alive: true)
+        book_1 = author_1.books.create!(name: "It", pages: 1011, fiction: true)
+        book_2 = author_1.books.create!(name: "The Shining", pages: 607, fiction: true)
+        visit "/books/#{book_1.id}"
+
+        expect(page).to have_content(book_1.name)
+        expect(page).to have_content(book_1.pages)
+        expect(page).to have_content(book_1.fiction)
+        expect(page).to_not have_content(book_2.name)
+        expect(page).to_not have_content(book_2.pages)
+    end
+
+end
