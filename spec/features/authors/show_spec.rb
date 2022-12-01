add622c074ed942598073cefef296984f7f6acce
Ryan Canton <ryancanton999@gmail.com>
2022-12-01 13:08:18 -0700

    Test: Add tests for user story 7

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe 'The Author show page' do
         expect(page).to_not have_content(author_2.rating)
         expect(page).to_not have_content(author_2.alive)
     end
+
+    it 'shows the count of chilrden' do
+        author_1 = Author.create!(name: "Stephen King", rating: 7, alive: true)
+        book_1 = author.books.create!(name: "It", pages: 1011, ficiton: true)
+        book_2 = author.books.create!(name: "The Shining", pages: 614, ficiton: true)
+        book_2 = author.books.create!(name: "Carrie", pages: 304, ficiton: true)
+        visit "/authors/#{author_1.id}"
+
+        expect(page).to have_content("Author Book Count: #{author_1.book_count}")
+    end
 end
