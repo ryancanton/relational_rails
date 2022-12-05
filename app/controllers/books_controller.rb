afcfc90d67d4f961409b33c7ce42a136088ed464
Ryan Canton <ryancanton999@gmail.com>
2022-12-05 15:10:31 -0700

    feat: add functionality to pass first test of user story 14

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,23 +3,11 @@ class BooksController < ApplicationController
       @books = Book.all
     end
 
-    def new
-
-    end
-
-    def create
-      book = Book.new({
-        name: params[:book][:name],
-        pages: params[:book][:pages],
-        fiction: params[:book][:fiction]
-        })
-  
-      book.save
-  
-      redirect_to '/books'
-    end
-
     def show
+      @book = Book.find(params[:id])
+    end
+
+    def edit
       @book = Book.find(params[:id])
     end
   end
