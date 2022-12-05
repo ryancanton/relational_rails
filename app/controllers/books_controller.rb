b61d54e0b4b0637e8d51be3025400bc30dd49818
Ryan Canton <ryancanton999@gmail.com>
2022-12-05 15:16:59 -0700

    Feat: Add functionality for user story 13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,15 @@ class BooksController < ApplicationController
     def edit
       @book = Book.find(params[:id])
     end
+
+    def update
+      book = Book.find(params[:id])
+      book.update(book_params)
+      redirect_to "/books/#{book.id}"
+    end
+
+  private
+    def book_params
+        params.permit(:name, :pages, :fiction)
+    end
   end
