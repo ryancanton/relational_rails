48ed52dd7f932197cad41d12dbfe138da0604e8d
Ryan Canton <ryancanton999@gmail.com>
2022-12-06 14:06:27 -0700

    Test: add test for user story 19

---
+++ spec/features/authors/destroy_spec.rb
@@ -0,0 +1,25 @@
+# As a visitor
+# When I visit a parent show page
+# Then I see a link to delete the parent
+# When I click the link "Delete Parent"
+# Then a 'DELETE' request is sent to '/parents/:id',
+# the parent is deleted, and all child records are deleted
+# and I am redirected to the parent index page where I no longer see this parent
+require 'rails_helper'
+
+RSpec.describe 'Deleting an author' do
+    it 'the show page contains a link that deletes the parent and all children' do
+        author_1 = Author.create!(name: "Stephen King", rating: 7, alive: true)
+        book_1 = author_1.books.create!(name: "It", pages: 1011, fiction: true)
+        book_2 = author_1.books.create!(name: "The Shining", pages: 607, fiction: true)
+        visit "/authors/#{author.id}"
+        click_link "Delete Stephen King"
+
+        expect(current_path).to eq("/authors")
+        expect(page).to_not have_content("Stephen King")
+
+        visit "/books"
+        expect(page).to_not have_content("It")
+        expect(page).to_not have_content("The Shining")
+    end
+end
